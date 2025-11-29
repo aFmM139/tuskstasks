fb7b2808d05c27f6401b2640173b3a44c37e7419
andyfajardo13092008 <andyfajardo13092008@gmail.com>
2025-11-29 05:33:26 +0000

    💄 ui(_layout):Se agregan mejores estilos a la app -Se agregan mejoras a la ui general de la app -Se oculta el menu de index -Se agrega tabs para la navegacion

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,33 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import { ListTodo, Cog } from 'lucide-react-native';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Tabs 
+      screenOptions={{ 
+        tabBarActiveTintColor: '#C4A784',
+        tabBarInactiveTintColor: '#6B7280',
+        tabBarStyle: {
+          backgroundColor: '#1A252F',
+          borderTopColor: '#374151',
+        },
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Tareas',
+          tabBarIcon: ({ color }) => <ListTodo size={24} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="ScreenConfiguration"
+        options={{
+          title: 'Configuración',
+          tabBarIcon: ({ color }) => <Cog size={24} color={color} />,
+        }}
+      />
+    </Tabs>
+  );
 }
